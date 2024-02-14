21c1aa879581a2db3b2c31125fbac0131101266a
mehrnaz <mehrnazmgh@gmail.com>
2024-02-14 11:55:21 +0330

    version 1 of telegram bot

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ keyboard.row('/start', '/help')
 def handle_start_help(message):
     sent = bot.send_message(message.chat.id, "لطفا یک گزینه را انتخاب کنید:", reply_markup=keyboard)
     bot.register_next_step_handler(sent, handle_all_messages)
-    # print(sent)
+    
 @bot.message_handler(func=lambda message: True)
 def handle_all_messages(message):
     if message.text == '/start':
@@ -30,20 +30,17 @@ def handle_all_messages(message):
     elif message.text == '/help':
         help_command(message)
         print("help")
-        # print(message)
     else:
-        # Handle other messages
+
         pass
 
     
-
 #commands
 @bot.message_handler(commands=['start'])
 def start_command(message):
     sent = bot.send_message(message.chat.id, 'سلام به بات تبدیل متن به صدا خوش آمدید.لطفا پیام خود را ارسال کنید')
     bot.register_next_step_handler(sent, handle_user_input)
-    # print(sent)
-
+    
 @bot.message_handler(commands=['help'])
 def help_command(message):
     bot.send_message(message.chat.id, 'این بات برای تبدیل متن به صدا ایجاد شده است.برای این منظور متن دلخواه خود را ارسال کنید و صدای متن را دریافت کنید.')
@@ -54,34 +51,36 @@ def handle_user_input(message):
     chat_id = message.chat.id
     user_input = message.text
     echo_all(message)  
+    
+
  
     #authentication in aipaa's api
-    responseFromAuthorizedAipaaApi = requests.post('https://api.aipaa.ir/auth/token' 
+    responseAuthorizedAipaaApi = requests.post('https://api.aipaa.ir/auth/token' 
                                                    , data= {
                 "client_id": "5KJ6sWa24IbYrXYYvWvmd3GtyJywGFLXTKR85EFK",
                 "client_secret": "U5eghdZdiVBrAI7lGolxu1tIE9EcvkKiFBsve8TyFy3wuuJhVJcx1AddGEcyGP49V6lpNuTf8ABGygoWTFHyZ8tjRvhDWcEG0G6UbrrB8fCkRFLbaHi0kF2tPnxiuZ6u",
                 "grant_type" : "client_credentials" 
             } , headers= { 'Content-Type' : 'application/x-www-form-urlencoded'})
-    print(responseFromAuthorizedAipaaApi)
+    print(responseAuthorizedAipaaApi)
 
-    accessTokenAipaa = responseFromAuthorizedAipaaApi.json()["access_token"]
+    accessTokenAipaa = responseAuthorizedAipaaApi.json()["access_token"]
 
     #create a voice tts service
-    responseFromVoiceTtsCreateAipaaApi = requests.post("https://api.aipaa.ir/api/v1/voice/tts/" 
+    responseVoiceTtsCreateAipaaApi = requests.post("https://api.aipaa.ir/api/v1/voice/tts/" 
                                                        , data= {
                 "input_text": user_input,
                 "sample_rate": 0 ,
                 "compress" : True 
             } , headers= { 'Authorization':'bearer '+ accessTokenAipaa })
 
-    getDowloadLinkFromTtsResponse = responseFromVoiceTtsCreateAipaaApi.json()["download_link"]
+    getDowloadLinkTtsResponse = responseVoiceTtsCreateAipaaApi.json()["download_link"]
 
     #api_v1_file_manager_file_download_read
-    responseFromFileManagerFileDownloadRead = requests.get( getDowloadLinkFromTtsResponse 
+    responseFileManagerFileDownloadRead = requests.get( getDowloadLinkTtsResponse 
                                                            , headers= { 'Authorization':'bearer '+ accessTokenAipaa } )
-    print(responseFromFileManagerFileDownloadRead)
+    print(responseFileManagerFileDownloadRead)
     with open('./voice.mp3', 'wb') as f:
-        f.write(responseFromFileManagerFileDownloadRead.content)
+        f.write(responseFileManagerFileDownloadRead.content)
         
     
     bot.send_audio(chat_id=chat_id, audio=open('./voice.mp3', 'rb'))
@@ -93,6 +92,7 @@ def echo_all(message):
     bot.send_message(message.chat.id, "ممنون از پیام شما! فایل صوتی تا لحظاتی دیگر برای شما ارسال می شود.")
     
 
+            
 
 bot.polling() 
 
