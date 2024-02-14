93a4906b5ed2c1e751218e2bf74ff71a4a5ec2f2
mehrnaz <mehrnazmgh@gmail.com>
2024-02-14 17:34:48 +0330

    wip

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ from telegram.ext import Application, CommandHandler
 import telebot
 from telebot import types
 import time
+import sqlite3
 
-        
+
+
+     
 #response from telegram server
 Token: Final = '6924394880:AAEikovwV-m_a4QV-3voXnvQkaZLI-PKDxY'
 BOT_USERNAME: Final = 'TSpeech_bot'
@@ -26,10 +29,10 @@ def handle_start_help(message):
 def handle_all_messages(message):
     if message.text == '/start':
         start_command(message)
-        print("start")
+        # print("start")
     elif message.text == '/help':
         help_command(message)
-        print("help")
+        # print("help")
     else:
 
         pass
@@ -49,43 +52,69 @@ def help_command(message):
     
 def handle_user_input(message):
     chat_id = message.chat.id
+    print(chat_id)
     user_input = message.text
     echo_all(message)  
     
+    
+    
+    conn = sqlite3.connect('text_to_speech.db')
+
+    conn.execute('''CREATE TABLE users
+            (chat_id TEXT PRIMARY KEY     NOT NULL,
+            download_count   INT    NOT NULL
+            )''')
+
+
+    for 
+    conn.execute("INSERT INTO users (chat_id,download_count) \
+        VALUES ('chat_id1', 1 )")
+
+    
+    cursor = conn.execute("SELECT chat_id, download_count from users")
+    for row in cursor:
+        print ("chat_id = ", row[0])
+        print ("download_count = ", row[1]), "\n"
+
+
+    conn.commit()
+    conn.close()
+
+
 
  
-    #authentication in aipaa's api
-    responseAuthorizedAipaaApi = requests.post('https://api.aipaa.ir/auth/token' 
-                                                   , data= {
-                "client_id": "5KJ6sWa24IbYrXYYvWvmd3GtyJywGFLXTKR85EFK",
-                "client_secret": "U5eghdZdiVBrAI7lGolxu1tIE9EcvkKiFBsve8TyFy3wuuJhVJcx1AddGEcyGP49V6lpNuTf8ABGygoWTFHyZ8tjRvhDWcEG0G6UbrrB8fCkRFLbaHi0kF2tPnxiuZ6u",
-                "grant_type" : "client_credentials" 
-            } , headers= { 'Content-Type' : 'application/x-www-form-urlencoded'})
-    print(responseAuthorizedAipaaApi)
+#     #authentication in aipaa's api
+#     responseAuthorizedAipaaApi = requests.post('https://api.aipaa.ir/auth/token' 
+#                                                    , data= {
+#                 "client_id": "5KJ6sWa24IbYrXYYvWvmd3GtyJywGFLXTKR85EFK",
+#                 "client_secret": "U5eghdZdiVBrAI7lGolxu1tIE9EcvkKiFBsve8TyFy3wuuJhVJcx1AddGEcyGP49V6lpNuTf8ABGygoWTFHyZ8tjRvhDWcEG0G6UbrrB8fCkRFLbaHi0kF2tPnxiuZ6u",
+#                 "grant_type" : "client_credentials" 
+#             } , headers= { 'Content-Type' : 'application/x-www-form-urlencoded'})
+#     print(responseAuthorizedAipaaApi)
 
-    accessTokenAipaa = responseAuthorizedAipaaApi.json()["access_token"]
+#     accessTokenAipaa = responseAuthorizedAipaaApi.json()["access_token"]
 
-    #create a voice tts service
-    responseVoiceTtsCreateAipaaApi = requests.post("https://api.aipaa.ir/api/v1/voice/tts/" 
-                                                       , data= {
-                "input_text": user_input,
-                "sample_rate": 0 ,
-                "compress" : True 
-            } , headers= { 'Authorization':'bearer '+ accessTokenAipaa })
+#     #create a voice tts service
+#     responseVoiceTtsCreateAipaaApi = requests.post("https://api.aipaa.ir/api/v1/voice/tts/" 
+#                                                        , data= {
+#                 "input_text": user_input,
+#                 "sample_rate": 0 ,
+#                 "compress" : True 
+#             } , headers= { 'Authorization':'bearer '+ accessTokenAipaa })
 
-    getDowloadLinkTtsResponse = responseVoiceTtsCreateAipaaApi.json()["download_link"]
+#     getDowloadLinkTtsResponse = responseVoiceTtsCreateAipaaApi.json()["download_link"]
 
-    #api_v1_file_manager_file_download_read
-    responseFileManagerFileDownloadRead = requests.get( getDowloadLinkTtsResponse 
-                                                           , headers= { 'Authorization':'bearer '+ accessTokenAipaa } )
-    print(responseFileManagerFileDownloadRead)
-    with open('./voice.mp3', 'wb') as f:
-        f.write(responseFileManagerFileDownloadRead.content)
+#     #api_v1_file_manager_file_download_read
+#     responseFileManagerFileDownloadRead = requests.get( getDowloadLinkTtsResponse 
+#                                                            , headers= { 'Authorization':'bearer '+ accessTokenAipaa } )
+#     print(responseFileManagerFileDownloadRead)
+#     with open('./voice.mp3', 'wb') as f:
+#         f.write(responseFileManagerFileDownloadRead.content)
         
     
-    bot.send_audio(chat_id=chat_id, audio=open('./voice.mp3', 'rb'))
-    time.sleep(5) # Sleep for 8 seconds
-    handle_start_help(message)
+#     bot.send_audio(chat_id=chat_id, audio=open('./voice.mp3', 'rb'))
+#     time.sleep(5) # Sleep for 8 seconds
+#     handle_start_help(message)
    
 
 def echo_all(message):
